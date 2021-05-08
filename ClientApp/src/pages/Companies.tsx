d7f75367cad57ad2d20b4ec3df23a2d5a7835da9
Ilya Ermolaev <Ermolaev.ID@yandex.ru>
2021-05-08 16:18:07 +0500

    Refactoring in Companies

--- ClientApp/src/pages/Companies.tsx
+++ ClientApp/src/pages/Companies.tsx
@@ -1,28 +1,39 @@
 import React, { useEffect, useState } from "react";
 import { CompanyDto } from "../common/Company.dto";
 import { CompanyList } from "../components/CompanyList/CompanyList";
+import { Loader } from "../components/Loader/Loader";
 import { useHttp } from "../hooks/http.hook";
 import { Wrapper } from "../styles/Companies.styles";
 
 export const Companies = () => {
   const req = useHttp();
-  const [data, setData] = useState<CompanyDto[]>([]);
+  const [data, setData] = useState<CompanyDto[]>();
+  const [loading, setLoading] = useState<boolean>(true);
 
   useEffect(() => {
     req<null, CompanyDto[]>({
       url: "sm/companies",
-      method: "GET",
-    }).then((e) => setData(e));
+    }).then(res => {
+      setData(res);
+      setLoading(false);
+    });
   }, []);
+
   return (
     <Wrapper>
-      {data.map((e) => (
-        <CompanyList
-          leftSide={e.title}
-          middleSide={e.stockPrice}
-          rightSide={e.ticker}
-        />
-      ))}
+      {loading ? (
+        <Loader />
+      ) : (
+        <>
+          {data?.map((e) => (
+            <CompanyList
+              title={e.title}
+              price={e.stockPrice}
+              ticker={e.ticker}
+            />
+          ))}
+        </>
+      )}
     </Wrapper>
   );
 };
--- ClientApp/src/styles/Companies.styles.ts
+++ ClientApp/src/styles/Companies.styles.ts
@@ -1,8 +1,11 @@
 import styled from "styled-components";
+import { useProportions } from "../hooks/windowProportions.hook";
+
+const { wHD, hHD } = useProportions();
 
 export const Wrapper = styled.div`
   display: flex;
+  width: calc(1140vw * ${wHD});
   flex-direction: column;
-  width: 100%;
   justify-content: center;
 `;
