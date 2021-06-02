28ade4ce3a460b02e3f397159257832010cd04aa
Ilya Ermolaev <Ermolaev.ID@yandex.ru>
2021-06-02 15:46:13 +0500

    Update Arrow component

--- ClientApp/src/components/Arrow/Arrow.tsx
+++ ClientApp/src/components/Arrow/Arrow.tsx
@@ -14,6 +14,7 @@ export const Arrow: React.FC<Props> = ({ direction, clickHandler }) => {
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
       onClick={clickHandler}
+      style={{ cursor: "pointer" }}
     >
       <path d={direction === "right" ? "M33 20L0 39.0526L0 0.947441L33 20Z" : "M0 20L33 0.947441V39.0526L0 20Z"} fill="black" />
     </svg>
